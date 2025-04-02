2f0d2482b82d618a66b9761d582c48d5e3db3ab8
“Mariana <marysiq@hotmail.com>
2025-04-02 13:48:51 +0100

    wip

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('converter')
+    return render_template('converter.html')
 
 @app.route('/convert', methods=['POST'])
 def convert():
