7291a8be7faa34e8636f340b5406d32799ab0a24
“Mariana <marysiq@hotmail.com>
2025-04-02 13:49:25 +0100

    wip

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def convert():
 
     if conversion_type == "temperature":
         result = f"{value}°F is {(value - 32) * 5 / 9:.2f}°C"
-    elif conversion_type == "weigt":
+    elif conversion_type == "weight":
         result = f"{value} lbs is {value * 0.453592:.2f} kg"
     elif conversion_type == "volume":
         result = f"{value} oz is {value * 29.5735:.2f} ml"
